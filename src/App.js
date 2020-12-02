d11d317a0b5357729a94422d360f8a08e629c259
Anand Ramdhani <anandkr.github@gmail.com>
2020-12-02 09:11:15 +0200

    First basic JS functionality added

--- src/App.js
+++ src/App.js
@@ -22,13 +22,31 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = () => {
+    console.log('Click')
+  }
+
   render() {
     return (
       <div className="App">
-        <Particles className='particles' params={particlesOptions} />
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         <Rank />
         {/* <FaceRecognition /> */}
       </div>
